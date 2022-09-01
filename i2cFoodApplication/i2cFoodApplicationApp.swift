3d5dbaeb36155c981dbf96919a9f5d12edf967de
Muhammad Ahmar <mhassan06@i2cinc.com>
2022-09-01 17:56:14 +0500

    Initial Commit

---
+++ i2cFoodApplication/i2cFoodApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  i2cFoodApplicationApp.swift
+//  i2cFoodApplication
+//
+//  Created by Muhammad Ahmar Hassan  on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct i2cFoodApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
